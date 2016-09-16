da43866fb3e323e57bf3737792e3dce675d941a5
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2016-09-16 21:18:58 +0900

    Update /etc/gemstash/config.yml by environment variables in entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,29 @@
 #! /bin/sh
 
+ruby -ryaml -e '
+  conf = YAML.load(File.read("/etc/gemstash/config.yml"))
+  ENV.each do |k,v|
+    if k =~ /^GEMSTASH_/
+      conf[k.sub(/^GEMSTASH_/,"").downcase.to_sym] =
+        case v
+        when "true"
+          true
+        when "false"
+          false
+        when "nil"
+          nil
+        when "false"
+          false
+        when /\A-?\d+\z/
+          v.to_i
+        else
+          v
+        end
+    end
+  end
+  File.open("/etc/gemstash/config.yml", "w") do |fh|
+    YAML.dump(conf, fh)
+  end
+'
+
 exec "$@"
